a6a6cedd455c952bb6bfe815949bb869f9487327
Admin <arayennet@gmail.com>
2024-11-10 14:43:09 +0200

    +

--- src/ZooInspector.js
+++ src/ZooInspector.js
@@ -39,27 +39,46 @@ class Inspection {
 
   inspectEnclosure(enclosure) {
     const enclosureImage = this.zoo.capturePictureOf(enclosure);
-      const enclosureStatus = this.imageRecognitionSystem.recognizeEnclosureStatus(enclosure, enclosureImage);
-      if (!enclosureStatus.isEnclosureSafe()) {
-        this.zoo.closeEnclosure(enclosure);
-        this.zoo.requestSecurityTo(enclosure);
-        this.zoo.requestMaintenanceCrewTo(enclosure);
-        this.addWarningToStatuses(enclosure, false);
-        this.zooWarningStatus = true;
-      }
+    const enclosureStatus = this.imageRecognitionSystem.recognizeEnclosureStatus(enclosure, enclosureImage);
+    if (this.isNotSafeEnclosure(enclosureStatus)) {
+      this.respondToNotSafeEnclosure(enclosure);
+      this.reportNotSafeEnclosureStatus(enclosure);
+    }
+  }
+
+  respondToNotSafeEnclosure(enclosure) {
+    this.zoo.closeEnclosure(enclosure);
+    this.zoo.requestSecurityTo(enclosure);
+    this.zoo.requestMaintenanceCrewTo(enclosure);
+  }
+
+  reportNotSafeEnclosureStatus(enclosure) {
+    this.addWarningToStatuses(enclosure, false);
+    this.zooWarningStatus = true;
+  }
+
+  isNotSafeEnclosure(enclosureStatus) {
+    return !enclosureStatus.isEnclosureSafe();
   }
 
   inspectAnimal(enclosure) {
     const animalImage = this.zoo.capturePictureOf(enclosure.getAnimal());
     const animalStatus = this.imageRecognitionSystem.recognizeAnimalStatus(enclosure.getAnimal(), animalImage);
     if (animalStatus.isAnimalSick()) {
-      this.zoo.closeEnclosure(enclosure);
-      this.zoo.requestVeterinaryTo(enclosure.getAnimal());
-      this.addWarningToStatuses(enclosure, true);
-      this.zooWarningStatus = true;
+      this.respondToSickAnimal(enclosure);
+      this.reportSickAnimalStatus(enclosure);
     }
   }
 
+  reportSickAnimalStatus(enclosure) {
+    this.addWarningToStatuses(enclosure, true);
+    this.zooWarningStatus = true;
+  }
+
+  respondToSickAnimal(enclosure) {
+    this.zoo.closeEnclosure(enclosure);
+    this.zoo.requestVeterinaryTo(enclosure.getAnimal());
+  }
 
   addWarningToStatuses(enclosure, isAnimal) {
     this.inspectionStatuses.push(`${isAnimal ? 'ANIMAL' : 'ENCLOSURE'}#${isAnimal ? enclosure.getAnimal().getName() : enclosure.getId()}#WARNING`);
